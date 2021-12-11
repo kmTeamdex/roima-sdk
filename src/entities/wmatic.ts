c1d36667138e04e6e64a21cded72716b3dc650ff
theplayer06 <manekiswap@gmail.com>
2021-12-11 11:05:44 +0700

    Pump version

--- src/entities/wmatic.ts
+++ src/entities/wmatic.ts
@@ -1,8 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import { SupportedChainId } from '..'
 import { Token } from './token'
 
 /**
  * Known WMATIC implementation addresses, used in our implementation of Matic#wrapped
  */
 export const WMATIC: { [chainId: number]: Token } = {
-  [137]: new Token(137, '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', 18, 'WMATIC', 'Wrapped Matic')
+  // eslint-disable-next-line prettier/prettier
+  [SupportedChainId.POLYGON]: new Token(137, '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', 18, 'WMATIC', 'Wrapped Matic'),
+  [SupportedChainId.MUMBAI]: new Token(137, '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889', 18, 'WMATIC', 'Wrapped Matic')
 }

cecc229e8233835589f630f9a69620c3eeba2cab
Lint Action <lint-action@samuelmeuli.com>
2021-12-11 04:06:26 +0000

    Fix code style issues with Prettier

--- src/entities/wmatic.ts
+++ src/entities/wmatic.ts
@@ -8,6 +8,12 @@ import { Token } from './token'
  */
 export const WMATIC: { [chainId: number]: Token } = {
   // eslint-disable-next-line prettier/prettier
-  [SupportedChainId.POLYGON]: new Token(137, '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', 18, 'WMATIC', 'Wrapped Matic'),
+  [SupportedChainId.POLYGON]: new Token(
+    137,
+    '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+    18,
+    'WMATIC',
+    'Wrapped Matic'
+  ),
   [SupportedChainId.MUMBAI]: new Token(137, '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889', 18, 'WMATIC', 'Wrapped Matic')
 }
